2521fd6abdc118d5bc9d7f25ab4407774e53cfeb
leonely1990 <leonel.wow.1990@gmail.com>
2022-09-05 21:34:05 -0400

    Front end del proyecto

---
+++ Front-End/recursos/js/allV2.js
@@ -0,0 +1,8 @@
+
+
+let titulo = document.title;
+    for (const elemento of document.getElementsByClassName("boton-menu")) {
+        if(titulo === elemento.innerHTML){
+            elemento.className += " active";
+        }
+    }
