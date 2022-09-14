6c886d5aacdfcf3a453e5d8d01dee05db536b40c
leonely1990 <leonel.wow.1990@gmail.com>
2022-09-13 20:40:54 -0400

    Popups de ver estado de pagos

--- Front-End/recursos/js/usuario.js
+++ Front-End/recursos/js/usuario.js
@@ -1,8 +1,18 @@
 const tipo = document.getElementById("tipo-correo");
 const enviar_correo = document.getElementById("enviar-correo");
-const cerrar = document.getElementById("cerrar").addEventListener("click", () => {
-    document.getElementsByClassName("popups")[0].style.display = "none";
-});
+const pagos = document.getElementById("verPagos");
+const cerrar = document.getElementsByClassName("cerrar");
+const prueba = document.getElementsByClassName("prueba");
+
+for (let i = 0; i < cerrar.length; i++) {
+    cerrar[i].addEventListener( "click", () => {
+        document.getElementsByClassName("popups")[0].style.display = "none";
+        for (let j = 0; j < prueba.length; j++) {
+            prueba[j].style.display = "none";            
+        }
+    });
+    
+}
 
 document.getElementById("seccion-"+tipo.value).style.display = "flex";
 
@@ -20,4 +30,10 @@ tipo.addEventListener("change", () => {
 
 enviar_correo.addEventListener("click", () => {
     document.getElementsByClassName("popups")[0].style.display = "flex";
+    document.getElementById("correo").style.display = "flex";
+});
+
+pagos.addEventListener("click", () => {
+    document.getElementsByClassName("popups")[0].style.display = "flex";
+    document.getElementById("pagos").style.display = "flex";
 });
