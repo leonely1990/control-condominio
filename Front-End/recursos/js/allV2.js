dbf583103e086fd5ff484ce5747b1969ace0fd41
leonely1990 <leonel.wow.1990@gmail.com>
2022-09-16 13:10:18 -0400

    Cambios en la barra lateral se cambiaron los before por la etiqueta img agregando estilos, se agregaron @media para menores resolucion

--- Front-End/recursos/js/allV2.js
+++ Front-End/recursos/js/allV2.js
@@ -1,8 +1,9 @@
 
 
 let titulo = document.title;
-    for (const elemento of document.getElementsByClassName("boton-menu")) {
-        if(titulo === elemento.innerHTML){
-            elemento.className += " active";
-        }
+
+for (const elemento of document.getElementsByClassName("boton-menu")) {
+    if(titulo === elemento.innerHTML){
+        elemento.parentNode.className += " active";
     }
+}
