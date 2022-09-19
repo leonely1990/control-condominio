3de46f9490fdaa0b3bb8580d302124b324864e10
leonely1990 <leonel.wow.1990@gmail.com>
2022-09-19 12:32:38 -0400

    se agrego el menu de movil y duncionalidad

--- Front-End/recursos/js/allV2.js
+++ Front-End/recursos/js/allV2.js
@@ -1,4 +1,4 @@
-
+let boton_menu_movil = document.getElementById("menu-movil__img");
 
 let titulo = document.title;
 
@@ -7,3 +7,21 @@ for (const elemento of document.getElementsByClassName("boton-menu")) {
         elemento.parentNode.className += " active";
     }
 }
+
+boton_menu_movil.addEventListener("click", () => {
+    console.log(document.getElementById("menu-movil").classList);
+    if(document.getElementById("menu-movil").classList.length > 0){
+        for(let i of document.getElementById("menu-movil").classList){
+            console.log("Entre al for");
+            if(i == "menu-active"){
+                console.log("primer if");
+                document.getElementById("menu-movil").className = "menu-deactive";
+            }else if(i == "menu-deactive"){
+                console.log("segundo if");
+                document.getElementById("menu-movil").className = "menu-active";
+            }
+        }
+    }else{
+        document.getElementById("menu-movil").className = "menu-active";
+    }
+});
