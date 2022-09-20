2969e3ca4e4b0d3dc834aed8c4b2bf947b456231
leonely1990 <leonel.wow.1990@gmail.com>
2022-09-20 15:50:55 -0400

    Cambios en el menu de movil

--- Front-End/recursos/js/allV2.js
+++ Front-End/recursos/js/allV2.js
@@ -1,4 +1,5 @@
 let boton_menu_movil = document.getElementById("menu-movil__img");
+let boton_cerrar_menu_movil = document.getElementById("cerrar-menu-movil");
 
 let titulo = document.title;
 
@@ -8,20 +9,22 @@ for (const elemento of document.getElementsByClassName("boton-menu")) {
     }
 }
 
+boton_cerrar_menu_movil.addEventListener("click", () => {
+    document.getElementById("menu-movil__div").className = "menu-deactive";
+});
+
 boton_menu_movil.addEventListener("click", () => {
-    console.log(document.getElementById("menu-movil").classList);
-    if(document.getElementById("menu-movil").classList.length > 0){
-        for(let i of document.getElementById("menu-movil").classList){
+    console.log(document.getElementById("menu-movil__div").classList);
+    if(document.getElementById("menu-movil__div").classList.length > 0){
+        for(let i of document.getElementById("menu-movil__div").classList){
             console.log("Entre al for");
             if(i == "menu-active"){
-                console.log("primer if");
-                document.getElementById("menu-movil").className = "menu-deactive";
+                document.getElementById("menu-movil__div").className = "menu-deactive";
             }else if(i == "menu-deactive"){
-                console.log("segundo if");
-                document.getElementById("menu-movil").className = "menu-active";
+                document.getElementById("menu-movil__div").className = "menu-active";
             }
         }
     }else{
-        document.getElementById("menu-movil").className = "menu-active";
+        document.getElementById("menu-movil__div").className = "menu-active";
     }
 });
